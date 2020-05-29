221695de60dc443361ea46f287347e89f5e0d1cd
Dmytro <dmytro.2004@gmail.com>
2020-05-29 19:31:53 +0300

    first commit

---
+++ js/script8.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const category = 'toys';
+const item = 4;
+
+console.log('https://someurl1.com/' + category + '/' + item);
+console.log(`https://someurl2.com/${category}/${item}`);
+console.log('https://someurl3.com/${category}/${item}');
+
+const user = "Dmytro";
+
+alert(`Hello, ${user}`);
---
+++ js/script9.js
@@ -0,0 +1,30 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(11%3);
+
+console.log(2*4 == '8');
+console.log(2*4 === '8');
+
+console.log(2 + 2 * 2 != '8');
+console.log(2 + 2 * 2 !== '8');
+console.log(2 + 2 * 2 !== '6');
+
+// &&
+// ||
+
+const isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
